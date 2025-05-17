3c01395c5f461929ac28f571a567004956f91831
Julian Hydroxl Cao <justpureh2o@outlook.com>
2025-05-17 14:05:27 +0800

    Update TableService.py

--- baad/lib/TableService.py
+++ baad/lib/TableService.py
@@ -3,18 +3,18 @@ from io import BytesIO
 from typing import IO
 from zipfile import ZipFile
 import os
-from .XXHashService import CalculateHash
+from .XXHashService import calculate_hash
 from typing import Union
 from .MersenneTwister import MersenneTwister
-import base64
+from base64 import b64encode
 
 class TableZipFile(ZipFile):
     def __init__(self, file: Union[str, BytesIO], name: str = None) -> None:
         super().__init__(file)
-        file_hash = CalculateHash(os.path.basename(file))                    # Calculate file hash with zipfile name
+        file_hash = calculate_hash(os.path.basename(file))                   # Calculate file hash with zipfile name
         rand = MersenneTwister(file_hash)                                    # Initialize a MT generator
         bts = rand.NextBytes(15)                                             # Generate a 15-byte key
-        self.password = base64.b64encode(bts)                                # Encode in BASE64 to get true password
+        self.password = b64encode(bts)                                       # Encode in BASE64 to get true password
         print(f'Password cracked: {self.password} for file ->{os.path.basename(file)}<-')
 
     def open(self, name: str, mode: str = "r", force_zip64=False):
