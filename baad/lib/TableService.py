0693150a3ad7de5818861270950ccc8505ef543b
Julian Hydroxl Cao <justpureh2o@outlook.com>
2025-05-17 13:50:25 +0800

    Update TableService.py

--- baad/lib/TableService.py
+++ baad/lib/TableService.py
@@ -1,28 +1,23 @@
-from base64 import b64encode
+# from _typeshed import StrPath
 from io import BytesIO
-from pathlib import Path
-from typing import Union
+from typing import IO
 from zipfile import ZipFile
-
+import os
+from .XXHashService import CalculateHash
+from typing import Union
 from .MersenneTwister import MersenneTwister
-from .XXHashService import calculate_hash
-
+import base64
 
 class TableZipFile(ZipFile):
-    def __init__(self, file: Union[str, BytesIO], password: bytes = None) -> None:
+    def __init__(self, file: Union[str, BytesIO], name: str = None) -> None:
         super().__init__(file)
-        if password is not None:
-            self.password = password
-            return
-            
-        file_name = Path(file).name if isinstance(file, str) else file.name
-        self.password = self._generate_password(file_name.lower())
+        file_hash = CalculateHash(os.path.basename(file))                    # Calculate file hash with zipfile name
+        rand = MersenneTwister(file_hash)                                    # Initialize a MT generator
+        bts = rand.NextBytes(15)                                             # Generate a 15-byte key
+        self.password = base64.b64encode(bts)                                # Encode in BASE64 to get true password
+        print(f'Password cracked: {self.password} for file ->{os.path.basename(file)}<-')
 
-    def _generate_password(self, file_name: str) -> bytes:
-        hash_value = calculate_hash(file_name)
-        twister = MersenneTwister(hash_value)
-        next_bytes = twister.next_bytes(15)
-        return b64encode(next_bytes)
-
-    def open(self, name: str, mode: str = 'r', force_zip64: bool = False) -> bytes:
-        return super().open(name, mode, pwd=self.password, force_zip64=force_zip64)
+    def open(self, name: str, mode: str = "r", force_zip64=False):
+        return super(self.__class__, self).open(
+            name, mode, pwd=self.password, force_zip64=force_zip64
+        )
